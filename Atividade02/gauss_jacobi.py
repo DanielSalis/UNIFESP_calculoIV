65f1ba56bdef4b7d153fc4524a7acbe025094c7e
DanielSalis <danielsalis98@gmail.com>
2022-12-06 22:44:38 -0300

    feat: first implementation

--- Atividade02/gauss_jacobi.py
+++ Atividade02/gauss_jacobi.py
@@ -1,2 +1,55 @@
-def run():
-    return True
+import math
+from tabulate import tabulate
+
+
+def compare(x, xk, eps):
+    current_sum = 0
+    arrays_tuple = zip(x, xk)
+    for x_i, xk_i in arrays_tuple:
+        current_sum += math.fabs(x_i-xk_i)
+
+    if (current_sum < eps):
+        return current_sum
+    else:
+        return False
+
+
+def run(A, b, max_iterations, eps):
+    saida = []
+    b_array_length = len(b)
+    has_soluction = True
+    x = b.copy()
+    for i in list(range(0, b_array_length, 1)):
+        if (math.fabs(A[i][i]) > 0.0):
+            x[i] = b[i]/A[i][i]
+        else:
+            has_soluction = False
+            break
+
+    if (has_soluction):
+        print("Iteração 0")
+        print("x = ", x)
+        xk = x.copy()
+        current_iteraction = 0
+
+        while (current_iteraction < max_iterations):
+            current_iteraction = current_iteraction + 1
+            for i in list(range(0, b_array_length, 1)):
+                current_sum = 0
+                for j in list(range(0, b_array_length, 1)):
+                    if (i != j):
+                        current_sum += A[i][j]*x[j]
+
+                xk[i] = (1/A[i][i])*(b[i]-current_sum)
+
+            print("Iteração: ", current_iteraction)
+            print("xk = ", xk)
+            current_eps = compare(x, xk, eps)
+            saida.append([current_iteraction, current_eps])
+            if current_eps:
+                x = xk.copy()
+                break
+            x = xk.copy()
+
+    print(tabulate(saida, headers=["k", "normres"]))
+    return x
--- Atividade02/main.py
+++ Atividade02/main.py
@@ -1,3 +1,10 @@
 from gauss_jacobi import run as gJacobi
 
-print(gJacobi())
+A = [[10, 2,  1],
+     [1, 5,  1],
+     [2, 3, 10]]
+
+b = [7, -8, 6]
+
+x = gJacobi(A, b, 1000, 0.0001)
+print("x = ", x)
