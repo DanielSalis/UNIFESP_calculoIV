fb9376f00d0f12aebda7b6503e77ca54dc3ee45d
DanielSalis <danielsalis98@gmail.com>
2022-12-07 20:07:12 -0300

    feat: add comments

--- Atividade02/main.py
+++ Atividade02/main.py
@@ -2,6 +2,8 @@ import numpy as np
 from gauss_jacobi import run as gJacobi
 from gauss_seidel import run as gSeidel
 
+# Questao 2
+
 
 def generate_matrix(alpha, beta, n):
     matrix = [[0]*n for _ in range(n)]
@@ -14,6 +16,8 @@ def generate_matrix(alpha, beta, n):
             else:
                 matrix[i][j] = 0
     return np.array(matrix)
+
+# Questao 2
 
 
 def generate_array_b(alpha, beta, n):
@@ -32,12 +36,15 @@ x_array = [0 for i in range(len(b_array))]
 
 print(A_matrix)
 print(b_array)
+print(x_array)
 
 # A = [[3, -1,  1],
 #      [1, 5,  -2],
 #      [2, -1, 4]]
 
 # b = [3, 4, 5]
+
+# Questao 3
 
 x = gJacobi(A_matrix, b_array, x_array, 2000, 0.00000001)
 print("x = ", x)
