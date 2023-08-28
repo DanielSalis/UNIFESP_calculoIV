4645e266388e2ba80b8ab9de4e147d97a083cf9d
DanielSalis <danielsalis98@gmail.com>
2023-08-28 14:36:37 -0300

    feat: add atividade 2

--- v2/atividade02.py
+++ v2/atividade02.py
@@ -30,7 +30,7 @@ def bisection(func, a, b, tolerance):
 
     return tabulate(saida, headers=["k", "xk", "f(xk)", 'stepk'])
 
-def newtonsMethod(function_statement, derivative_statement, x, n):
+def newtonsMethod(function_statement, derivative_statement, x, max_iteractions):
     iteration = 0
     saida = []
 
@@ -42,7 +42,7 @@ def newtonsMethod(function_statement, derivative_statement, x, n):
         d = eval(derivative_statement)
         return d
 
-    while abs(function(x)) >= 0.000001 and iteration <= n:
+    while abs(function(x)) >= 0.000001 and iteration <= max_iteractions:
         saida.append([iteration,x,function(x)])
         i = x - (function(x) / derivative(x))
         x = i
@@ -50,7 +50,34 @@ def newtonsMethod(function_statement, derivative_statement, x, n):
 
     return tabulate(saida, headers=["iteration", "x", "f(x)"])
 
+def secantMethod(func, x0, x1, tolerance, max_iteractions):
+    def f(x):
+        f = eval(func)
+        return f
+
+    saida = []
+    for iteraction in range(1, max_iteractions, 1):
+        fx0 = f(x0)
+        fx1 = f(x1)
+        if abs(fx1) < tolerance:
+            break
+        xi = x1 - fx1*((x1-x0)/(fx1-fx0))
+        fxi = f(xi)
+        saida.append([iteraction, xi, fxi, fx0])
+        x0 = x1
+        x1 = xi
+
+    return tabulate(saida, headers=["iteraction", "xi", "f(xi)", "fx0"])
+
 # print(newtonsMethod("x**2 - 2 ", "2*x", 2, 10) + '\n')
 print(newtonsMethod("x**3 - x**2 - 1 ", "3*x**2 - 2*x", 2, 10) + '\n')
 print(bisection("x**3 - x**2 - 1 ", 0, 2, 0.000001) + '\n')
 print("Podemos perceber que o método da bissecção demorou 20 iterações enquanto o de newton apenas 4")
+
+print("\nAtividade 2 - 2")
+print("\nBisecção\n", bisection("x**2 - 7", 1, 3, 0.000001))
+print("\nNewton\n", newtonsMethod("x**2 - 7", "2*x", 7, 100))
+print("\nSecante\n", secantMethod("x**2 - 7", 1, 3, 0.000001, 100))
+print("Podemos perceber que o método da secante demorou 4 iterações, o de newton também 4 e o da bissecção demorou 20")
+
+print("\nAtividade 2 - 3")
