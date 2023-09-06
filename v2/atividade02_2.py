174eaddc998099dfef9c1b91914715bdb3d3de16
DanielSalis <danielsalis98@gmail.com>
2023-09-06 00:09:42 -0300

    fix: remove teste

--- v2/atividade02_2.py
+++ v2/atividade02_2.py
@@ -1,6 +1,8 @@
 from tabulate import tabulate
 import numpy as np
 import math
+import matplotlib.pyplot as plt
+
 
 def cps_equation(A):
     h = 290
@@ -127,3 +129,46 @@ print("Utilizando o método da bissecção conseguimos encontrar o valor: " + st
 print("\n"+ str(casos[2]))
 print("Utilizando o método da secante conseguimos encontrar o valor: " + str(secantMethod(equacao_colebrook_3, 0.0001, 0.0003, 1e-6, 100)))
 print("Utilizando o método da bissecção conseguimos encontrar o valor: " + str(bisectionMehod(equacao_colebrook_3, 0.0001, 1, 1e-6)))
+
+print("\nAtividade 2_2 - 4")
+
+def parabola_equation(x):
+    return x**2 + 1
+
+def elipse_equation(x):
+    return 4 * np.sqrt(1 - (x**2))
+
+x_values = np.linspace(-1.0, 1.0, 120, dtype='float64')
+
+y_parabola = parabola_equation(x_values)
+y_elipse = elipse_equation(x_values)
+
+intersection_points_x = []
+intersection_points_y = []
+
+for x, y_p, y_e in zip(x_values, y_parabola, y_elipse):
+    if np.isclose(round(y_p,1), round(y_e,1)):
+        intersection_points_x.append(x)
+        intersection_points_y.append(y_p)
+
+# Plotar as curvas
+plt.figure(figsize=(8, 6))
+plt.plot(x_values, y_parabola, label='Parábola: $y = x^2 + 1$')
+plt.plot(x_values, y_elipse, label='Elipse: $\\frac{x^2}{1} + \\frac{y^2}{4} = 1$')
+plt.scatter(intersection_points_x, intersection_points_y, color='red', label='Pontos de Interseção')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.grid()
+plt.title('Interseção entre Parábola e Elipse')
+plt.axhline(0, color='black',linewidth=0.5)
+plt.axvline(0, color='black',linewidth=0.5)
+plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
+plt.xlim(-1.5, 1.5)
+plt.ylim(-0.5, 2.5)
+plt.show()
+
+# Exibir os pontos de interseção
+print("Pontos de Interseção:")
+for x, y in zip(intersection_points_x, intersection_points_y):
+    print(f"({x:.4f}, {y:.4f})")
