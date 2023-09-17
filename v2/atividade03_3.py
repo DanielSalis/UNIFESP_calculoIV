c605db99250a471ba84643cf6bbdd089a23cf974
DanielSalis <danielsalis98@gmail.com>
2023-09-17 18:51:46 -0300

    fix: text

--- v2/atividade03_3.py
+++ v2/atividade03_3.py
@@ -45,13 +45,13 @@ print("Atividade 3 - parte 1; Exercício 1:")
 n2 = 3
 A2 = np.array([[1, 1, 1],
                [1, 1, 2],
-               [2, 3, 5]], dtype=float)
+               [2, 3, 5]], dtype=int)
 
-b2 = np.array([400, 600, 1500], dtype=float)
+b2 = np.array([400, 600, 1500], dtype=int)
 x2 = np.zeros(n2)
 A_b2 = criar_matriz_aumentada(A2, b2)
 gauss_jacobi(A_b2, x2)
-# print("Não foi possível a conversão\nDevido ao fato de que a matriz A2 não atende aos critérios de diagonal estritamente dominante ou diagonal dominante em todas as linhas, não podemos garantir a convergência do método de Gauss-Jacobi para este sistema.")
+print("Não foi possível a conversão\nDevido ao fato de que a matriz A2 não atende aos critérios de diagonal estritamente dominante ou diagonal dominante em todas as linhas, não podemos garantir a convergência do método de Gauss-Jacobi para este sistema.")
 # Para a primeira linha: 1 > 1 + 1 (não é estritamente dominante).
 # Para a segunda linha: 1 > 1 + 2 (não é estritamente dominante).
 # Para a terceira linha: 5 > 2 + 3 (é estritamente dominante).
