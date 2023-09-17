fb00dd5bbaeec2691b7c7a329fc8f468f33cf419
DanielSalis <danielsalis98@gmail.com>
2023-09-17 15:12:45 -0300

    feat: add ex 1

--- v2/atividade03_3.py
+++ v2/atividade03_3.py
@@ -1,37 +1,42 @@
 import numpy as np
 
-def gauss_jacobi(A, b, tol=1e-6, max_iter=50):
-    n = len(b)
-    x = np.zeros(n)
-    x_new = np.zeros(n)
-    iterations = 0
+def criar_matriz_aumentada(matriz, vetor):
+    n = matriz.shape[0]
+    new_matrix = np.column_stack((matriz, vetor.reshape(n, 1)))
+    print(new_matrix)
+    return new_matrix
 
-    while iterations < max_iter:
-        for i in range(n):
-            x_new[i] = (b[i] - np.dot(A[i, :i], x[:i]) - np.dot(A[i, i+1:], x[i+1:])) / A[i, i]
+def gauss_jacobi(A_b, x):
+    sum_j = 0
+    for i in range(len(x)):
+      for j in range(i + 1, len(x)):
+          sum_j = sum_j + j
+          A_b[j, :] = A_b[j, :] - (A_b[j, i] / A_b[i, i]) * A_b[i, :]
+    print("iterações:" + str(sum_j))
 
-        print(x_new)
+    for i in reversed(range(len(x))):
+        x[i] = (A_b[i, -1] - np.dot(A_b[i, i+1:len(x)], x[i+1:len(x)])) / A_b[i, i]
 
-        if np.all(np.abs(x_new - x) < tol):
-            return x_new  # Retorna o vetor solução se convergir
 
-        x = x_new.copy()
-        iterations += 1
+print("Atividade 3 - parte 1; Exercício 1:")
+A = np.array([[1, -1, 0, 5],
+              [3, -2, 1, -1],
+              [1, 1, 9, 4],
+              [1, -7, 2, 3]], dtype=float)
+b = np.array([18, 8, 47, 32], dtype=float)
+n = 4
+x = np.zeros(n)
+A_b = criar_matriz_aumentada(A, b)
+gauss_jacobi(A_b, x)
+print("Vetor solução x:"+ str(x) + "\n")
 
-    print("O método não convergiu após {} iterações.".format(max_iter))
-    return None
-
-# Exemplo de uso:
-
-A_matrix = np.array([[1, -1, 0, 5],
-                     [3, -2, 1, -1],
-                     [1, 1, 9, 4],
-                     [1,-7,2,3]])
-b_array = np.array([18,8,47,32])
-x_array = [0 for i in range(len(b_array))]
-
-solucao = gauss_jacobi(A_matrix, b_array)
-
-if solucao is not None:
-    print("A solução do sistema é:")
-    print(solucao)
+# Teste com outro exemplo
+# A_2 = np.array([[10, 2,  1],
+#      [ 1, 5,  1],
+#      [ 2, 3, 10]], dtype=float)
+# b_2 = np.array([7, -8, 6], dtype=float)
+# n_2 = 3
+# x_2 = np.zeros(n_2)
+# A_b_2 = criar_matriz_aumentada(A_2, b_2)
+# gauss_jacobi(A_b_2, x_2)
+# print("Vetor solução x:", x_2)
