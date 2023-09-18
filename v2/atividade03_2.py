19430c6e632c479812a13c559638902af1dffedb
DanielSalis <danielsalis98@gmail.com>
2023-09-18 13:56:08 -0300

    feat: add atividade 3 part 3

--- v2/atividade03_2.py
+++ v2/atividade03_2.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def criar_matriz_aumentada(matriz, vetor):
     n = matriz.shape[0]
@@ -19,17 +20,67 @@ def gauss_jacobi(A_b, x):
 
 
 
-print("Atividade 3 - parte 1; Exercício 1:")
+print("Atividade 3 - parte 2; Exercício 1:")
 n2 = 3
-A2 = np.array([[1, 1, 1],
-               [1, 1, 2],
-               [2, 3, 5]], dtype=int)
+A2 = np.array([[70, 20, 10],
+               [10, 90, 0],
+               [15, 10, 75]], dtype=int)
 
-b2 = np.array([400, 600, 1500], dtype=int)
+b2 = np.array([4000, 1000, 2000], dtype=int)
 x2 = np.zeros(n2)
 A_b2 = criar_matriz_aumentada(A2, b2)
 gauss_jacobi(A_b2, x2)
-print("Não foi possível a conversão\nDevido ao fato de que a matriz A2 não atende aos critérios de diagonal estritamente dominante ou diagonal dominante em todas as linhas, não podemos garantir a convergência do método de Gauss-Jacobi para este sistema.")
-# Para a primeira linha: 1 > 1 + 1 (não é estritamente dominante).
-# Para a segunda linha: 1 > 1 + 2 (não é estritamente dominante).
-# Para a terceira linha: 5 > 2 + 3 (é estritamente dominante).
+print("Vetor solução x:"+ str(x2) + "\n")
+
+print("Atividade 3 - parte 2; Exercício 3:")
+# Given data
+T = np.array([0, 5, 10, 15, 20, 25])
+C = np.array([14.5, 12.6, 11.3, 10.3, 9.1, 8.5])
+
+plt.scatter(T, C, label='Data Points', color='black')
+plt.title('Plot dos pontos')
+plt.xlabel('T')
+plt.ylabel('C')
+plt.show()
+
+# Método de newton
+def divided_difference(x, y):
+    n = len(x)
+    F = np.zeros((n, n))
+    F[:, 0] = y
+
+    for j in range(1, n):
+        for i in range(n - j):
+            F[i][j] = (F[i + 1][j - 1] - F[i][j - 1]) / (x[i + j] - x[i])
+
+    return F
+
+# Coefficients of the interpolation polynomial
+coefficients = divided_difference(T, C)[0]
+
+# Function to evaluate the interpolation polynomial
+def interpolate(x, coefficients, T):
+    result = coefficients[0]
+    for i in range(1, len(coefficients)):
+        term = coefficients[i]
+        for j in range(i):
+            term *= (x - T[j])
+        result += term
+    return result
+
+# Generate points on the interpolation polynomial
+x_values = np.linspace(0, 25, 100)
+y_values = [interpolate(x, coefficients, T) for x in x_values]
+
+# Plot the given data points
+plt.scatter(T, C, label='Data Points', color='red')
+
+# Plot the interpolation polynomial
+plt.plot(x_values, y_values, label='Interpolation Polynomial', color='blue')
+
+plt.xlabel('T')
+plt.ylabel('C')
+plt.legend()
+plt.title('Interpolação com método de newton')
+plt.grid(True)
+plt.show()
