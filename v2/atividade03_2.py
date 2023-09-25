1dd084a680d3181105f872ab15029e2b7df22c36
DanielSalis <danielsalis98@gmail.com>
2023-09-25 13:37:59 -0300

    feat: add ex 5

--- v2/atividade03_2.py
+++ v2/atividade03_2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sympy as sp
 
 def criar_matriz_aumentada(matriz, vetor):
     n = matriz.shape[0]
@@ -18,7 +19,27 @@ def gauss_jacobi(A_b, x):
     for i in reversed(range(len(x))):
         x[i] = (A_b[i, -1] - np.dot(A_b[i, i+1:len(x)], x[i+1:len(x)])) / A_b[i, i]
 
-# print("Atividade 3 - parte 2; Exercício 1:")
+def divided_difference(x, y):
+    n = len(x)
+    F = np.zeros((n, n))
+    F[:, 0] = y
+
+    for j in range(1, n):
+        for i in range(n - j):
+            F[i][j] = (F[i + 1][j - 1] - F[i][j - 1]) / (x[i + j] - x[i])
+
+    return F
+
+def interpolate(x, coefficients, T):
+    result = coefficients[0]
+    for i in range(1, len(coefficients)):
+        term = coefficients[i]
+        for j in range(i):
+            term *= (x - T[j])
+        result += term
+    return result
+
+print("Atividade 3 - parte 2; Exercício 1:")
 n2 = 3
 A2 = np.array([[70, 20, 10],
                [10, 90, 0],
@@ -31,10 +52,8 @@ gauss_jacobi(A_b2, x2)
 print("Vetor solução x:"+ str(x2) + "\n")
 
 print("Atividade 3 - parte 2; Exercício 3:")
-# Given data
 T = np.array([0, 5, 10, 15, 20, 25])
 C = np.array([14.5, 12.6, 11.3, 10.3, 9.1, 8.5])
-
 plt.scatter(T, C, label='Data Points', color='black')
 plt.title('Plot dos pontos')
 plt.xlabel('T')
@@ -43,41 +62,12 @@ plt.show()
 
 print("------ || ------\n")
 
-# Método de newton
-def divided_difference(x, y):
-    n = len(x)
-    F = np.zeros((n, n))
-    F[:, 0] = y
-
-    for j in range(1, n):
-        for i in range(n - j):
-            F[i][j] = (F[i + 1][j - 1] - F[i][j - 1]) / (x[i + j] - x[i])
-
-    return F
-
-# Coefficients of the interpolation polynomial
 coefficients = divided_difference(T, C)[0]
-
-# Function to evaluate the interpolation polynomial
-def interpolate(x, coefficients, T):
-    result = coefficients[0]
-    for i in range(1, len(coefficients)):
-        term = coefficients[i]
-        for j in range(i):
-            term *= (x - T[j])
-        result += term
-    return result
-
-# Generate points on the interpolation polynomial
 x_values = np.linspace(0, 25, 100)
 y_values = [interpolate(x, coefficients, T) for x in x_values]
 
-# Plot the given data points
 plt.scatter(T, C, label='Data Points', color='red')
-
-# Plot the interpolation polynomial
 plt.plot(x_values, y_values, label='Interpolation Polynomial', color='blue')
-
 plt.xlabel('T')
 plt.ylabel('C')
 plt.legend()
@@ -86,16 +76,11 @@ plt.grid(True)
 plt.show()
 
 print("Atividade 3 - parte 2; Exercício 4:")
-# Given data
 T_data = np.array([605, 685, 725, 765, 825, 855, 875])
 C_data = np.array([0.622, 0.655, 0.688, 0.679, 0.730, 0.907, 1.336])
-
 coefficients = divided_difference(T_data, C_data)[0]
-
 T_values_to_estimate = [645, 795, 845]
-
 C_estimated = [interpolate(T, coefficients, T_data) for T in T_values_to_estimate]
-
 known_values = [0.639, 0.694, 0.812]
 
 for i, T_value in enumerate(T_values_to_estimate):
@@ -113,3 +98,19 @@ plt.legend()
 plt.title('Interpolação exercício 4')
 plt.grid(True)
 plt.show()
+
+
+print("Atividade 3 - parte 2; Exercício 5")
+x, x0, x1, x2 = sp.symbols('x x0 x1 x2')
+f_x = 2 * x**2 * sp.exp(x) + 1
+x_values = [1, 0.5, 1]
+f_values = [f_x.subs(x, x_val) for x_val in x_values]
+
+L0 = (x - x1) * (x - x2) / ((x0 - x1) * (x0 - x2))
+L1 = (x - x0) * (x - x2) / ((x1 - x0) * (x1 - x2))
+L2 = (x - x0) * (x - x1) / ((x2 - x0) * (x2 - x1))
+
+P_x = f_values[0] * L0 + f_values[1] * L1 + f_values[2] * L2
+
+approximation = P_x.subs(x, 0.8).evalf()
+print("Aproximação de f(0.8):", approximation)
